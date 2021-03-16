1c187a0893748b7cee2d0b7fc9a7590541bfdf6f
Taylor Keazirian <taylork26@gmail.com>
2021-03-16 10:06:33 -0500

    changed to vercel link

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  API_ENDPOINT: 'http://localhost:8000'
+  // API_ENDPOINT: 'http://localhost:8000'
+  API_ENDPOINT: 'https://noteful-five-puce.vercel.app/'
 }
 
 export default config;
